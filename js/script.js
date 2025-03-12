1827feda94dabce407ae2b9e171b8e51f60c5592
Lovisa <lovisa.przybylak@gmail.com>
2025-03-12 12:09:36 +0100

    Förändringar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 button = document.getElementById('nav-icon');
 button.addEventListener('click', function() { 
-    this.classList.toggle('open');}
+    this.classList.toggle('open');
+    showMenu();}
 )
+
+
+let layer = document.querySelector(".fade-layer");
+layer.addEventListener("click", showMenu);
+
+
+function showMenu(){
+    let menu = document.querySelector("nav.hamburger");
+    menu.classList.toggle("show");
+
+    let layer = document.querySelector(".fade-layer");
+    layer.classList.toggle("visible");
+
+}
