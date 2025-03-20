9546376e21a37f3d2d40e57fc893f83148f43a97
Lovisa <lovisa.przybylak@gmail.com>
2025-03-20 13:42:29 +0100

    problem

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-button = document.getElementById('nav-icon');
+const hamMenu = document.querySelector('#nav-icon');
+
+const offScreen = document.querySelector('.off-screen');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreen.classList.toggle('active');
+})
+
+
+/*button = document.getElementById('nav-icon');
 button.addEventListener('click', function() { 
     this.classList.toggle('open');
     showMenu();}
 )
 
 
-let layer = document.querySelector(".fade-layer");
-layer.addEventListener("click", showMenu);
-
-
 function showMenu(){
-    let menu = document.querySelector("nav.hamburger");
+    let menu = document.querySelector('nav.off-screen');
     menu.classList.toggle("show");
 
     let layer = document.querySelector(".fade-layer");
-    layer.classList.toggle("visible");
+    layer.classList.toggle('visible');
 
-}
+} */
