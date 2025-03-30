61059d71c6141c4c512c63171cfd5b68753bb5eb
Lovisa <lovisa.przybylak@gmail.com>
2025-03-30 23:59:50 +0200

    inlämning

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".styrelsebilder img").forEach(img => {
+        let container = document.createElement("div");
+        container.classList.add("image-container");
+
+        let text = document.createElement("span");
+        text.classList.add("image-text");
+        text.innerText = img.alt; // Använd alt-texten för att fylla texten
+
+        img.parentNode.insertBefore(container, img);
+        container.appendChild(img);
+        container.appendChild(text);
+    });
+});
 
